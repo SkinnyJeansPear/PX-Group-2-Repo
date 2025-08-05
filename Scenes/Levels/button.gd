5491f4ddbf8cfe8a222336348349d59eed88950f
22090614 <153805962+SkinnyJeansPear@users.noreply.github.com>
2025-08-05 10:09:23 +1000

    Cleaned up workspace

--- Scenes/Levels/button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-@onready var grid: TileMapLayer = $"../../Grid"
-
-func _on_toggled(button_pressed):
-	if button_pressed == true:
-		grid.visible = true
-	else:
-		grid.visible = false
