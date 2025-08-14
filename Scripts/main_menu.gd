e3eaafb05fdfac893234a9c55345a9fa72ccb4df
22090614 <153805962+SkinnyJeansPear@users.noreply.github.com>
2025-08-14 14:16:34 +1000

    changed file name

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_start_tutorial_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/Test.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/Tutorial.tscn")
