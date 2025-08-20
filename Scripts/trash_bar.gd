2115171c38d9a6ca2e2f8052a8dc09503a57bba6
Jana <99482204+janabee3@users.noreply.github.com>
2025-08-20 23:05:27 +1000

    Added object trashing sound

--- Scripts/trash_bar.gd
+++ Scripts/trash_bar.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var bin: TextureRect = $TextureRect/Bin
 @onready var hidden_pos = position
 @onready var shown_pos = position - Vector2(200, 0)  # Slide left by 200 px
+@onready var trashed_object: AudioStreamPlayer = $TextureRect/Bin/trashed_object
+
 var tween: Tween
 
 func _ready():
@@ -20,6 +22,7 @@ func hide_bar():
 		tween.kill()
 	tween = create_tween()
 	tween.tween_property(self, "position", hidden_pos, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	trashed_object.play()
 
 func is_mouse_over_bin() -> bool:
 	return bin.get_global_rect().has_point(get_global_mouse_position())
