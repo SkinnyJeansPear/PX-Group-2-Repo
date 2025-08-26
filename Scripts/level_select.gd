fdc4192c36edf369a6f06970eb827f9a2e90ef27
Jana <99482204+janabee3@users.noreply.github.com>
2025-08-26 21:38:38 +1000

    Design, Sound, & Objects
    Panels have been designed to include a scroll design.
    Titles have been added, and a font has been added for design.
    Extra objects have been added to tutorial. Note that all wont be added, only for proper levels.
    Sounds have been implemented for the rest of the buttons.

--- Scripts/level_select.gd
+++ Scripts/level_select.gd
@@ -1,9 +1,17 @@
 extends Control
+@onready var menu_click: AudioStreamPlayer = $menu_click
 
+func _play_click_and_change_scene(scene_path: String) -> void:
+	menu_click.play()
+	await menu_click.finished
+	get_tree().change_scene_to_file(scene_path)
 
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
-
+	_play_click_and_change_scene("res://Scenes/main_menu.tscn")
 
 func _on_parking_lot_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/ParkingLot.tscn")
+	_play_click_and_change_scene("res://Scenes/Levels/ParkingLot.tscn")
+
+# when adding the rest of the scenes, do it like this format
+#func _on_parking_lot_pressed() -> void:
+	#_play_click_and_change_scene("res://Scenes/Levels/ParkingLot.tscn")
